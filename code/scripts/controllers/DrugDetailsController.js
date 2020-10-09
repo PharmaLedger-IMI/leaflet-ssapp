5640f3f104e0c4b72363c262f643212fd8663ce6
Balakrishna <balakrishna.chandrasekaran@novartis.com>
2020-10-09 16:12:29 +0530

    Modified code and added assets

--- code/scripts/controllers/DrugDetailsController.js
+++ code/scripts/controllers/DrugDetailsController.js
@@ -3,13 +3,21 @@ import ContainerController from "../../cardinal/controllers/base-controllers/Con
 export default class DrugDetailsController extends ContainerController {
     constructor(element, history) {
         super(element, history);
-
+this.setModel({});
         this.DSUStorage.getItem('/tmp/batch/product/product.json', 'json', (err, product) => {
             if (err) {
                 console.log(err);
             }
             product.photo = '/download/tmp/batch/product'+product.photo;
-            this.setModel({product});
+            this.model.product=product;
         });
+
+        this.DSUStorage.getItem('package', 'json', (err, packages) => {
+            if (err) {
+                console.log(err);
+            }
+            this.model.packages=packages
+            console.log( "Packages here: ",packages)
+        });        
     }
 }
