d5aafd9971c53de4cac71c7af41666ffe46ab1ac
skutner <sava.dumitru.daniel@gmail.com>
2020-10-28 15:31:21 +0200

    small improvements

--- code/scripts/controllers/HistoryController.js
+++ code/scripts/controllers/HistoryController.js
@@ -1,40 +1,51 @@
 import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+import utils from "../../utils.js";
 
 export default class HistoryController extends ContainerController {
     constructor(element, history) {
         super(element);
-        console.log("Preparing to set up the view model");
-
         this.setModel({});
 
-        const self = this;
-        this.model.addExpression('historyLoaded', function() {
-            console.log("Expression checking", typeof self.model.scannedDrugs !== "undefined");
-            return typeof self.model.scannedDrugs !== "undefined";
-        }, 'scannedDrugs');
+        this.on("view-leaflet", (event) => {
+            let target = event.target;
+            let targetLabel = target.shadowRoot.querySelector("slot").assignedElements()[0].innerHTML;
+            const regex = /[\d]+/gm;
+            const index = regex.exec(targetLabel);
+        }, {capture: true});
 
-        this.DSUStorage.getItem('/data/drugsHistory.json', 'json', (err, drugHistory) => {
-            if(typeof drugHistory === "undefined"){
-                return this.model.scannedDrugs = [];
+        this.DSUStorage.call("listDSUs", "/packages", (err, dsuList) => {
+            const products = [];
+            const __readProductsRecursively = (packageNumber, callback) => {
+                if (packageNumber < dsuList.length) {
+                    this.DSUStorage.getItem(`/packages/${dsuList[packageNumber].path}/batch/product/product.json`, 'json', (err, product) => {
+                        if (err) {
+                            return callback(err);
+                        }
+
+                        this.DSUStorage.getItem(`/packages/${dsuList[packageNumber].path}/package.json`, 'json', (err, pack) => {
+                            if (err) {
+                                return callback(err);
+                            }
+
+                            product.expiry = utils.getDate(pack.expiration);
+                            product.photo = '/download/tmp/batch/product' + product.photo;
+                            products.push(product);
+                            packageNumber++;
+                            __readProductsRecursively(packageNumber, callback);
+                        });
+                    });
+                } else {
+                    callback(undefined, products);
+                }
             }
 
-            this.model.scannedDrugs = drugHistory;
-        });
-
-        this.on("clearHistory", (event) => {
-            this.DSUStorage.setItem('/data/drugsHistory.json', JSON.stringify([]), err => {
-                if (!err) {
-                    this.model.scannedDrugs = [];
+            __readProductsRecursively(0, (err, productsList) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    this.model.products = productsList;
                 }
             });
         });
-
-        this.on("view-drug", (event) => {
-            history.push("/drug-details");
-        }, {capture: true});
-
-        this.on("displayLeaflet", (event) => {
-            history.push("/leaflet");
-        }, {capture: true});
     }
 }
