75fc3408fece0c2d969fcc2e0572938ee4d4ba7e
Irimia Cosmin-Iulian <irimia.cosmin@gmail.com>
2020-11-23 14:39:18 +0200

    First working version of SMPC on drug details

--- code/scripts/controllers/ScanErrorController.js
+++ code/scripts/controllers/ScanErrorController.js
@@ -3,6 +3,9 @@ import ContainerController from '../../cardinal/controllers/base-controllers/Con
 export default class ScanErrorController extends ContainerController {
     constructor(element, history) {
         super(element, history);
-        this.setModel(history.location.state);
+        this.setModel({
+            ... history.location.state,
+            title: 'Product not found'
+        });
     }
 }
