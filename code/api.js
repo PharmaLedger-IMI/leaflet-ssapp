3d81c87d64aef790ec0542554483b3f806afe08c
skutner <sava.dumitru.daniel@gmail.com>
2021-04-29 15:58:34 +0300

    removed mainDSU refresh calls

--- code/api.js
+++ code/api.js
@@ -2,22 +2,11 @@ const securityContext = require("opendsu").loadApi("sc");
 const mainDSU = securityContext.getMainDSU();
 
 function mountDSU(path, keySSI, callback) {
-    //TODO: check if this is still useful
-    mainDSU.refresh((err) => {
-        if (err) {
-            return callback(err);
-        }
-        mainDSU.mount(path, keySSI, callback);
-    });
+    mainDSU.mount(path, keySSI, callback);
 }
 
 function listDSUs(path, callback) {
-    mainDSU.refresh((err) => {
-        if (err) {
-            return callback(err);
-        }
-        mainDSU.listMountedDossiers(path, callback);
-    });
+    mainDSU.listMountedDossiers(path, callback);
 }
 
 function loadDSU(keySSI, callback) {
@@ -29,12 +18,7 @@ function listFolders(path, callback) {
     if (path.endsWith("/")) {
         path = path.slice(0, -1);
     }
-    mainDSU.refresh((err) => {
-        if (err) {
-            return callback(err);
-        }
-        mainDSU.listFolders(path, {ignoreMounts: false}, callback);
-    });
+    mainDSU.listFolders(path, {ignoreMounts: false}, callback);
 }
 
 function refreshDSUMountedAtPath(path, callback) {
