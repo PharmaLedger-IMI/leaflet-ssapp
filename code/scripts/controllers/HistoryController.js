e6da019ff237eddce22dc8aba8ac9fd5dc6910dd
adrian.ganga <adrian.ganga@heavensolutions.com>
2021-10-25 18:23:28 +0300

    Update history page to new design

--- code/scripts/controllers/HistoryController.js
+++ code/scripts/controllers/HistoryController.js
@@ -1,37 +1,99 @@
 const {WebcController} = WebCardinal.controllers;
-const {DataSource} = WebCardinal.dataSources;
 import constants from "../../constants.js";
 import SettingsService from "../services/SettingsService.js";
 import BatchStatusService from "../services/BatchStatusService.js";
 import DSUDataRetrievalService from "../services/DSUDataRetrievalService/DSUDataRetrievalService.js";
 import utils from "../../utils.js";
 
-class HistoryDataSource extends DataSource {
-  constructor(...props) {
-    const [myService, ...defaultOptions] = props;
-    super(...defaultOptions);
-    this.setPageSize(2);
-    this.settingsService = myService;
+export default class HistoryController extends WebcController {
+  constructor(element, history) {
+    super(element, history);
+    this.model = {products: [], itemsOnPage: 4};
+    this.model.settingsService = new SettingsService(this.DSUStorage);
+
+    let self = this;
+    this.model.settingsService.readSetting("refreshPeriod", (err, refreshPeriod) => {
+      if (err || !refreshPeriod) {
+        refreshPeriod = constants.DEFAULT_REFRESH_PERIOD;
+      }
+      setInterval(async () => {
+        await self.getPageDataAsync();
+      }, refreshPeriod * 1000)
+    });
+
+    setTimeout(async () => {
+      const pageTemplate = document.querySelector('webc-app-loader[tag="home"] page-template');
+      await pageTemplate.componentOnReady();
+      const ionContent = pageTemplate.shadowRoot.querySelector('ion-content');
+      await ionContent.componentOnReady();
+      ionContent.scrollEvents = true;
+      await self.getPageDataAsync();
+      ionContent.addEventListener('ionScrollStart', async (e) => {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        let fake_item = self.element.querySelector('#last-fake-item');
+        if (fake_item && self.isInViewport(fake_item)) {
+          await self.getPageDataAsync();
+        }
+      })
+    }, 0)
+
+
+    this.onTagClick("view-details", (model, target, event) => {
+      let dbApi = require("opendsu").loadApi("db");
+      dbApi.getMainEnclaveDB((err, enclaveDB) => {
+        if (err) {
+          console.log('Error on getting enclave DB');
+          return;
+        }
+        enclaveDB.getRecord(constants.HISTORY_TABLE, model.pk, (err, record) => {
+          if (err) {
+            console.log("Could not find record for pk: ", model.pk);
+            return;
+          }
+
+          this.navigateToPageTag("drug-details", {
+            productData: record
+          })
+        })
+      })
+    });
+
   }
 
-  async getPageDataAsync(startOffset, dataLengthForCurrentPage) {
+  isInViewport(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  async getPageDataAsync() {
 
     let dbApi = require("opendsu").loadApi("db");
-    let products = [];
-    let advancedUser = await this.settingsService.asyncReadSetting("advancedUser");
+    let advancedUser = await this.model.settingsService.asyncReadSetting("advancedUser");
     this.advancedUser = !!advancedUser;
-    let refreshPeriod = await this.settingsService.asyncReadSetting("refreshPeriod");
+    let refreshPeriod = await this.model.settingsService.asyncReadSetting("refreshPeriod");
     this.secondsToUpdate = refreshPeriod || constants.DEFAULT_REFRESH_PERIOD;
 
     try {
       this.dbStorage = await $$.promisify(dbApi.getMainEnclaveDB)();
-      let results = await $$.promisify(this.dbStorage.filter)(constants.HISTORY_TABLE, "createdAt > 0", "dsc");
-      this.setRecordsNumber(results.length);
+
+      if (this.model.products.length && this.model.products[this.model.products.length - 1].lastFakeItem) {
+        this.model.products.pop();
+      }
+      let lastCreatedAt = this.model.products.length ? this.model.products[this.model.products.length - 1].createdAt : new Date().toISOString();
+
+      let results = await $$.promisify(this.dbStorage.filter)(constants.HISTORY_TABLE, `createdAt < ${lastCreatedAt}`, "dsc", this.model.itemsOnPage);
+
       if (results && results.length > 0) {
         if (document.querySelector(".datatable-title") && document.querySelector(".datatable-title").hidden) {
           document.querySelector(".datatable-title").hidden = false;
         }
-        results = results.slice(startOffset, startOffset + dataLengthForCurrentPage);
+
         for (let result of results) {
           if (Date.now() - result['__timestamp'] > this.secondsToUpdate * 1000) {
             let batchData;
@@ -58,7 +120,7 @@ class HistoryDataSource extends DataSource {
           }
           result.advancedView = this.advancedUser;
         }
-        products = results;
+        this.model.products = [...this.model.products, ...results];
       } else {
         if (document.querySelector(".datatable-title") && document.querySelector(".datatable-title").hidden) {
           document.querySelector(".datatable-title").hidden = true;
@@ -69,7 +131,7 @@ class HistoryDataSource extends DataSource {
     }
     //group by month and year
     let groups = {};
-    for (let product of products) {
+    for (let product of this.model.products) {
       let date = product.createdAt.slice(0, 7);
       if (!groups[date]) {
         groups[date] = [];
@@ -84,53 +146,10 @@ class HistoryDataSource extends DataSource {
 
       groups[date].push(product);
     }
-    return products;
+    if (this.model.products.length > 0 && this.model.products.length >= this.model.itemsOnPage) {
+      this.model.products.push({lastFakeItem: true});
+    }
+    return this.model.products;
   }
 }
 
-export default class HistoryController extends WebcController {
-  constructor(element, history) {
-    super(element, history);
-
-    let settingsService = new SettingsService(this.DSUStorage);
-    this.model = {
-      productsDataSource: new HistoryDataSource(settingsService)
-    }
-    const {productsDataSource} = this.model;
-
-    settingsService.readSetting("refreshPeriod", (err, refreshPeriod) => {
-      if (err || !refreshPeriod) {
-        refreshPeriod = constants.DEFAULT_REFRESH_PERIOD;
-      }
-      setInterval(() => {
-        productsDataSource.goToPageByIndex(0)
-      }, refreshPeriod * 1000)
-    });
-
-    let dbApi = require("opendsu").loadApi("db");
-    let viewDetails = (pk) => {
-
-      dbApi.getMainEnclaveDB((err, enclaveDB) => {
-        if (err) {
-          console.log('Error on getting enclave DB');
-          return;
-        }
-        enclaveDB.getRecord(constants.HISTORY_TABLE, pk, (err, record) => {
-          if (err) {
-            console.log("Could not find record for pk: ", pk);
-            return;
-          }
-
-          this.navigateToPageTag("drug-details", {
-            productData: record
-          })
-        })
-      })
-    }
-
-    this.onTagClick("view-details", (model, target, event) => {
-      viewDetails(model.pk);
-    });
-
-  }
-}
