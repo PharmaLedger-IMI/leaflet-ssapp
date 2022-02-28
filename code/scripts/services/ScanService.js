46783957fd301bb4bc234e640e80925d8904218c
Deny Pătrașcu <denypatrascu@gmail.com>
2022-02-28 18:55:04 +0200

    Add Pharma UI to the newer Scan Service

--- code/scripts/services/ScanService.js
+++ code/scripts/services/ScanService.js
@@ -1,5 +1,84 @@
 import Scanner from "../../lib/zxing-wrapper/scanner.js";
 
+function createOverlay([x, y, w, h], canvasDimensions) {
+	const canvas = document.createElement("canvas");
+	canvas.id = "overlay";
+	canvas.width = canvasDimensions.width;
+	canvas.height = canvasDimensions.height;
+	canvas.style.position = "absolute";
+	canvas.style.top = "50%";
+	canvas.style.left = "50%";
+	canvas.style.transform = "translate(-50%, -50%)";
+
+	const context = canvas.getContext("2d");
+	context.lineWidth = 6;
+	context.strokeStyle = "rgb(255, 255, 255)";
+	context.fillStyle = "rgba(0, 0, 0, 0.5)";
+
+	const r = 20;
+
+	const clearBackdrop = (x, y, w, h, r) => {
+		context.save();
+		context.beginPath();
+
+		context.moveTo(x + r, y);
+		context.lineTo(x + w - r, y);
+		context.quadraticCurveTo(x + w, y, x + w, y + r);
+		context.lineTo(x + w, y + h - r);
+		context.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+		context.lineTo(x + r, y + h);
+		context.quadraticCurveTo(x, y + h, x, y + h - r);
+		context.lineTo(x, y + r);
+		context.quadraticCurveTo(x, y, x + r, y);
+
+		context.clip();
+		context.clearRect(x, y, w, h);
+		context.restore();
+	};
+
+	const drawBackdrop = (x, y, w, h) => {
+		context.fillRect(x, y, w, h);
+	};
+
+	const drawCorners = (x, y, w, h) => {
+		const p = [
+			new Path2D("M53 3H19C10.1634 3 3 8.84767 3 16.0612V43"),
+			new Path2D("M53 43L53 15.8C53 8.73072 45.6904 3 36.6735 3L3 3"),
+			new Path2D("M3 43L37 43C45.8366 43 53 37.1523 53 29.9388L53 3"),
+			new Path2D("M3 3L3 30.2C3 37.2693 10.3096 43 19.3265 43L53 43"),
+		];
+
+		// dimensions of Path2D
+		const d = {
+			l: context.lineWidth,
+			w: 50,
+			h: 40,
+		};
+
+		context.translate(x, y);
+		context.stroke(p[0]);
+
+		context.translate(w - d.w - d.l, 0);
+		context.stroke(p[1]);
+
+		context.translate(0, h - d.h - d.l);
+		context.stroke(p[2]);
+
+		context.translate(d.w + d.l - w, 0);
+		context.stroke(p[3]);
+
+		context.resetTransform();
+	};
+
+	drawBackdrop(0, 0, canvasDimensions.width, canvasDimensions.height);
+
+	clearBackdrop(x, y, w, h, r);
+
+	drawCorners(x, y, w, h);
+
+	return canvas;
+}
+
 function nativeLayerAvailable() {
 	let result = false;
 	try {
@@ -13,8 +92,14 @@ function nativeLayerAvailable() {
 export default class ScanService {
 	constructor(domElement) {
 		this.scanner = new Scanner(domElement);
+
 		//todo: remove this changeWorker call when possible
 		this.scanner.changeWorker("lib/zxing-wrapper/worker/zxing-0.18.6-worker.js");
+
+		this.scanner.drawOverlay = (centralPoints, canvasDimensions) => {
+			const overlay = createOverlay(centralPoints, canvasDimensions);
+			domElement.append(overlay)
+		}
 
 		this.usingNativeLayer = nativeLayerAvailable();
 	}
