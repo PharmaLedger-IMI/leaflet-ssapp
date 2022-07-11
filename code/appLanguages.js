3fe2d704140958c6d3e38bb26292c06f4d0c6560
adrian <adiganga2002@yahoo.com>
2022-07-11 10:08:46 +0300

    #608 Missing texts for mobile app (update translate keys and add new languages)

--- code/appLanguages.js
+++ code/appLanguages.js
@@ -2,18 +2,40 @@ export default {
   en: [
     {value: "en", label: "English", nativeName: "English"},
     {value: "de", label: "German", nativeName: "Deutsch"},
+    {value: "fr", label: "French", nativeName: "Français"},
+    {value: "nl", label: "Dutch", nativeName: "Nederlands, Vlaams"},
     {value: "ro", label: "Romanian", nativeName: "Românā"}
   ],
   ro: [
     {value: "en", label: "Englezā", nativeName: "English"},
     {value: "de", label: "Germanā", nativeName: "Deutsch"},
+    {value: "fr", label: "Franceză", nativeName: "Français"},
+    {value: "nl", label: "Olandeză", nativeName: "Nederlands, Vlaams"},
     {value: "ro", label: "Românā", nativeName: "Românā"}
   ],
   de: [
     {value: "en", label: "Englisch", nativeName: "English"},
     {value: "de", label: "Deutsch", nativeName: "Deutsch"},
+    {value: "fr", label: "Französisch", nativeName: "Français"},
+    {value: "nl", label: "Niederländisch", nativeName: "Nederlands, Vlaams"},
     {value: "ro", label: "Rumänisch", nativeName: "Românā"}
+  ],
+  nl: [
+    {value: "en", label: "Engels", nativeName: "English"},
+    {value: "de", label: "Duits", nativeName: "Deutsch"},
+    {value: "fr", label: "Frans", nativeName: "Français"},
+    {value: "nl", label: "Nederlands, Vlaams", nativeName: "Nederlands, Vlaams"},
+    {value: "ro", label: "Roemeense", nativeName: "Românā"}
+  ],
+  fr: [
+    {value: "en", label: "Anglais", nativeName: "English"},
+    {value: "de", label: "Allemand", nativeName: "Deutsch"},
+    {value: "fr", label: "Français", nativeName: "Français"},
+    {value: "nl", label: "Néerlandais", nativeName: "Nederlands, Vlaams"},
+    {value: "ro", label: "Roumain", nativeName: "Românā"}
   ]
-
 }
+
+
+
 
--- code/scripts/controllers/OnBoardingController.js
+++ code/scripts/controllers/OnBoardingController.js
@@ -3,29 +3,34 @@ import SettingsService from "../services/SettingsService.js";
 const {WebcController} = WebCardinal.controllers;
 
 export default class OnBoardingController extends WebcController {
-    constructor(...props) {
-        super(...props);
-        this.model = {firstStep: true}
-        this.onTagClick("get-started", (model, target, event) => {
-            this.model.firstStep = false;
-        });
-        this.onTagClick("agree-terms", (model, target, event) => {
-            let dbApi = require("opendsu").loadApi("db");
-            dbApi.getMainEnclaveDB(async (err, enclaveDB) => {
-                if (err) {
-                    console.log('Error on getting enclave DB');
-                    return;
-                }
+  constructor(...props) {
+    super(...props);
+    this.model = {firstStep: true}
+    this.onTagClick("get-started", (model, target, event) => {
+      this.model.firstStep = false;
+      this.model.confirmDisagree = false;
+    });
+    this.onTagClick("agree-terms", (model, target, event) => {
+      let dbApi = require("opendsu").loadApi("db");
+      dbApi.getMainEnclaveDB(async (err, enclaveDB) => {
+        if (err) {
+          console.log('Error on getting enclave DB');
+          return;
+        }
 
-                let settingsService = new SettingsService(enclaveDB);
-                await settingsService.asyncWriteSetting("onbordingComplete", true);
+        let settingsService = new SettingsService(enclaveDB);
+        await settingsService.asyncWriteSetting("onbordingComplete", true);
 
-                this.navigateToPageTag("home");
-            })
-        })
-        this.onTagClick("disagree-terms", (model, target, event) => {
-            this.model.firstStep = true;
-        })
-    }
+        this.navigateToPageTag("home");
+      })
+    })
+    this.onTagClick("disagree-terms", (model, target, event) => {
+      this.querySelector(".terms-and-conditions").scrollTo({top: 0, behavior: 'smooth'});
+      this.model.confirmDisagree = true;
+    })
+    this.onTagClick("disagree-terms-confirm", (model, target, event) => {
+      this.model.firstStep = true;
+    })
+  }
 
 }
