828882b0ae1c1ff561bc7bd27e2c52f211115a6d
adrian.ganga <adiganga2002@gmail.com>
2022-09-06 17:07:08 +0300

    #868 Integrate dev branch from iOS (jailbreak detection, WebSockets)

--- code/scripts/controllers/LandingController.js
+++ code/scripts/controllers/LandingController.js
@@ -5,21 +5,48 @@ const {WebcController} = WebCardinal.controllers;
 export default class OnBoardingController extends WebcController {
   constructor(...props) {
     super(...props);
+    this.model = {loading: true}
+
+    let host = window.location.host;
+    fetch(`http://${host}/jailbreak/details`)
+      .then(response => response.text())
+      .then(textString => {
+        console.log(textString);
+        this.model.loading = false;
+        if (textString) {
+          this.showModalFromTemplate('error-message', () => {
+          }, () => {
+            this.initialize();
+          }, {
+            model: {
+              title: this.translate("_warning"),
+              subtitle: this.translate("_subtitle"),
+              messageText: this.translate("jailbroken_msg")
+            }, disableExpanding: true, disableFooter: true
+          });
+        } else {
+          this.initialize();
+        }
+      }).catch(err => {
+      this.model.loading = false;
+      this.initialize();
+    });
+  }
+
+  initialize() {
     let dbApi = require("opendsu").loadApi("db");
     dbApi.getMainEnclaveDB(async (err, enclaveDB) => {
       if (err) {
         console.log('Error on getting enclave DB');
         return;
       }
-
       let settingsService = new SettingsService(enclaveDB);
-      let onbordingComplete = await settingsService.asyncReadSetting("onbordingComplete");
-
+      let onbordingComplete = await settingsService.asyncReadSetting("onbordingComplete")
       if (!onbordingComplete) {
         this.navigateToPageTag("onboarding")
       } else {
         this.navigateToPageTag("home")
       }
-    })
+    });
   }
 }
