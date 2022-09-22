76ab5d98446ff9627e425cd00650cb7d96a732b4
adrian.ganga <adiganga2002@gmail.com>
2022-09-22 09:39:34 +0300

    #126 Terms of use and Privacy statement

--- code/scripts/controllers/PrivacyPolicyController.js
+++ code/scripts/controllers/PrivacyPolicyController.js
@@ -3,7 +3,9 @@ const {WebcController} = WebCardinal.controllers;
 export default class HelpController extends WebcController {
   constructor(...props) {
     super(...props);
-    this.model.pageContent = `<div class="label">{{$section_title}}</div>
-                    <div class="content">{{ $privacy_policy }}</div>               `
+    this.model.pageContent = `
+        <iframe style="width: 100%; height: 100%; font-family: 'DM Sans regular'; border: 0" src="https://app.termly.io/document/privacy-policy/40430c0a-9e30-4690-b4a7-2c2f56433919"></iframe>
+
+                             `
   }
 }
--- code/scripts/controllers/TermsAndConditionsController.js
+++ code/scripts/controllers/TermsAndConditionsController.js
@@ -3,7 +3,8 @@ const {WebcController} = WebCardinal.controllers;
 export default class TermsAndConditionsController extends WebcController {
   constructor(...props) {
     super(...props);
-    this.model.pageContent = `<div class="label">{{$section_title}}</div>
-                    <div class="content">{{ $terms_and_conditions }}</div>               `
+    this.model.pageContent = `
+    <iframe style="width: 100%; height: 100%; font-family: 'DM Sans regular'; border: 0" src="https://app.termly.io/document/terms-of-use-for-website/76a94ec4-766a-4a6a-b7fe-c68834af6811"></iframe>
+    `
   }
 }
